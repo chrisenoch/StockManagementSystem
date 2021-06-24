858a1aa9eadf51ce6f305a908d29fa583ad2cb12
Christopher Enoch <christopher.enoch@yahoo.co.uk>
2021-06-24 12:20:19 +0200

    App and Sorting_Java8Streams classes now refactored and working correctly.

--- src/main/java/com/chris/enoch/shoeshop/demo/App.java
+++ src/main/java/com/chris/enoch/shoeshop/demo/App.java
@@ -1,15 +1,26 @@
 package com.chris.enoch.shoeshop.demo;
 
-import com.chris.enoch.shoeshop.LoliShoeShop;
+import static com.chris.enoch.shoeshop.entity.StockToBeOrderedUtils.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import com.chris.enoch.shoeshop.entity.Address;
 import com.chris.enoch.shoeshop.entity.Shoe;
 import com.chris.enoch.shoeshop.entity.ShoeType;
+import com.chris.enoch.shoeshop.entity.Stock;
+import com.chris.enoch.shoeshop.entity.StockToBeOrdered;
+import com.chris.enoch.shoeshop.entity.StockToBeOrderedShopA;
 import com.chris.enoch.shoeshop.entity.Supplier;
-import com.chris.enoch.shoeshop.exceptions.TooManyTypesOfShoesException;
-import com.chris.enoch.shoeshop.exceptions.TooMuchStockException;
 
 public class App {
 	public static void main(String[] args) {
+		//Init Stock
+		Stock stock = new Stock(new ArrayList<>());
+		
+		final int MAX_STOCK_CAN_HOLD = 500;
+		final int MAX_NUM_OF_SHOE_TYPES = 10;
+		
 		//Init addresses
 		
 		Address nikeAddress = new Address(1, (short)14, "Paul Street", "Harbourne", "Birmingham"
@@ -25,80 +36,41 @@ public class App {
 		Supplier vansSupplier = new Supplier(2, vansAddress);
 		
 		//Init shoes - Improve variable names			
-		Shoe trainer = new Shoe(0, 0, nikeSupplier, ShoeType.RUNNING, 8000);	
-		Shoe flipFlops = new Shoe(1, 1, vansSupplier, ShoeType.BEACH, 5000);
-		Shoe hikingBoots = new Shoe(2, 2, doctorMartinsSupplier, ShoeType.MILITARY, 9000);
-		Shoe nikeAirtrainerShoe = new Shoe(3,  3, nikeSupplier, ShoeType.SNEAKER, 11000);
-		Shoe leatherbootsVans = new Shoe(4,  4, vansSupplier, ShoeType.LEATHER_BOOTS, 1200);
-		Shoe r9s = new Shoe(5,  5, nikeSupplier, ShoeType.FOOTBALL_BOOTS, 15000);
-		Shoe balletShoes = new Shoe(5,  5, vansSupplier, ShoeType.DANCING, 8000);
-		Shoe sandals = new Shoe(6,  6, vansSupplier, ShoeType.WEDDING, 20000);
-		Shoe rugbyNike = new Shoe(7,  7, nikeSupplier, ShoeType.RUGBY, 8000);
-		Shoe wellies = new Shoe(8,  8, doctorMartinsSupplier, ShoeType.WELLIES, 5000);
-		Shoe slippers = new Shoe(9,  9, doctorMartinsSupplier, ShoeType.SLIPPERS, 2000);
+		Shoe trainer = new Shoe(0, 0, "SomeTrainer", nikeSupplier, ShoeType.RUNNING, 8000);	
+		Shoe flipFlops = new Shoe(1, 1, "SomeFlipFlops", vansSupplier, ShoeType.BEACH, 5000);
+		Shoe hikingBoots = new Shoe(2, 2, "SomeHikingBoots", doctorMartinsSupplier, ShoeType.MILITARY, 9000);
+		Shoe nikeAirtrainerShoe = new Shoe(3,  3, "SomeNikeShoe", nikeSupplier, ShoeType.SNEAKER, 11000);
+		Shoe leatherbootsVans = new Shoe(4,  4, "SomeLeatherBootsVans", vansSupplier, ShoeType.LEATHER_BOOTS, 1200);
+		Shoe r9s = new Shoe(5,  5, "SomeR9s", nikeSupplier, ShoeType.FOOTBALL_BOOTS, 15000);
+		Shoe sandals = new Shoe(6,  6, "SomeSandals", vansSupplier, ShoeType.WEDDING, 20000);
+		Shoe rugbyNike = new Shoe(7,  7, "SomeNikeRugbyShoes", nikeSupplier, ShoeType.RUGBY, 8000);
+		Shoe wellies = new Shoe(8,  8, "SomeWellies", doctorMartinsSupplier, ShoeType.WELLIES, 5000);
+		Shoe slippers = new Shoe(9,  9, "SomeSlipeprs", doctorMartinsSupplier, ShoeType.SLIPPERS, 2000);
+		Shoe balletShoes = new Shoe(10,  10, "SomeBalletShoes", vansSupplier, ShoeType.DANCING, 8000);
 
-		LoliShoeShop loliShoeShop = new LoliShoeShop();
+		List<Shoe> stockToBeOrdererd = new ArrayList<>();
+		StockToBeOrdered stockToBeOrdered = new StockToBeOrderedShopA (stockToBeOrdererd, stock, trainer, 400
+				, MAX_STOCK_CAN_HOLD, MAX_NUM_OF_SHOE_TYPES);
+		 
+		System.out.println(addAndGetFeedback(stockToBeOrdered, trainer, 400));
+		System.out.println(addAndGetFeedback(stockToBeOrdered, trainer, 120)); //Should fail
+		System.out.println(addAndGetFeedback(stockToBeOrdered, flipFlops, 1));
+		System.out.println(addAndGetFeedback(stockToBeOrdered, hikingBoots, 2));
+		System.out.println(addAndGetFeedback(stockToBeOrdered, nikeAirtrainerShoe, 3));
+		System.out.println(addAndGetFeedback(stockToBeOrdered, leatherbootsVans, 3));
+		System.out.println(addAndGetFeedback(stockToBeOrdered, r9s, 4));
+		System.out.println(addAndGetFeedback(stockToBeOrdered, balletShoes, 5));
+		System.out.println(addAndGetFeedback(stockToBeOrdered, sandals, 6));
+		System.out.println(addAndGetFeedback(stockToBeOrdered, rugbyNike, 7));
+		System.out.println(addAndGetFeedback(stockToBeOrdered, wellies, 2));
 		
-		//Test stockLessThanOrEqualTo500#
-		try {
-			loliShoeShop.orderStock(trainer, 400);
-			loliShoeShop.orderStock(trainer, 110);
-		} catch (TooManyTypesOfShoesException | TooMuchStockException e) {
-			e.getMessage();
-			System.out.println(e.getMessage());
-		}	
-		
-		System.out.println("Should be 400: " + loliShoeShop.getStockList().size()); //Should be 400
-		
-		//Test stockLessThanOrEqualTo500#
-		//Should work
-		try {
-			loliShoeShop.orderStock(flipFlops, 1);
-			loliShoeShop.orderStock(hikingBoots, 1);
-			loliShoeShop.orderStock(nikeAirtrainerShoe , 1);
-			loliShoeShop.orderStock(leatherbootsVans, 1);
-			loliShoeShop.orderStock(r9s, 1);
-			loliShoeShop.orderStock(balletShoes, 1);
-			loliShoeShop.orderStock(sandals, 1);
-			loliShoeShop.orderStock(slippers, 1);
-			loliShoeShop.orderStock(wellies, 10);	
-
-		} catch (TooManyTypesOfShoesException | TooMuchStockException e) {
-			e.getMessage();
-			System.out.println(e.getMessage());
-		}	
-		System.out.println("Should be 418: " + loliShoeShop.getStockList().size()); //Should be 418
-
-		//Should throw exception as is the 11th type of shoe to be added and only 10 types allowed.
-		try {	
-			loliShoeShop.orderStock(rugbyNike, 1);
-
-		} catch (TooManyTypesOfShoesException | TooMuchStockException e) {
-			e.getMessage();
-			System.out.println(e.getMessage());
-		}
-		System.out.println("Should be 418: " +  loliShoeShop.getStockList().size()); //Should be 418
+		//Next one should fail
+		System.out.println("Size of stockToBeOrderedList " + stockToBeOrdered.getStockToBeOrdered().size());
+		System.out.println("Next one should fail.");	
+		System.out.println(addAndGetFeedback(stockToBeOrdered, slippers, 2)); //Not working
 		
 		
-		//Test buyShoes#
-		//Should work
-		try {
-			loliShoeShop.buyShoes(wellies, 10);
-		} catch (Exception e) {
-			e.getMessage();
-			System.out.println(e.getMessage());
-		}
-		System.out.println("Should be 408: " + loliShoeShop.getStockList().size()); //Should be 408
-		
-		//Should fail as only 10 pairs of wellies in stock
-		try {
-			loliShoeShop.buyShoes(wellies, 1);
-		} catch (Exception e) {
-			e.getMessage();
-			System.out.println(e.getMessage());
-		}
-		System.out.println("Should be 408: " + loliShoeShop.getStockList().size()); //Should be 408
-		
+		System.out.println("Size of stockToBeOrderedList " + stockToBeOrdered.getStockToBeOrdered().size());
 
 	}
 
--- src/main/java/com/chris/enoch/shoeshop/demo/Sorting_Java8Streams.java
+++ src/main/java/com/chris/enoch/shoeshop/demo/Sorting_Java8Streams.java
@@ -1,6 +1,7 @@
 package com.chris.enoch.shoeshop.demo;
 
 import static java.util.Comparator.comparing;
+import static com.chris.enoch.shoeshop.entity.StockToBeOrderedUtils.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -10,15 +11,13 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import com.chris.enoch.shoeshop.LoliShoeShop;
 import com.chris.enoch.shoeshop.entity.Address;
 import com.chris.enoch.shoeshop.entity.Shoe;
 import com.chris.enoch.shoeshop.entity.ShoeType;
+import com.chris.enoch.shoeshop.entity.Stock;
 import com.chris.enoch.shoeshop.entity.StockToBeOrdered;
 import com.chris.enoch.shoeshop.entity.StockToBeOrderedShopA;
 import com.chris.enoch.shoeshop.entity.Supplier;
-import com.chris.enoch.shoeshop.exceptions.TooManyTypesOfShoesException;
-import com.chris.enoch.shoeshop.exceptions.TooMuchStockException;
 
 import static java.util.stream.Collectors.*;
 
@@ -26,56 +25,59 @@ import static java.util.stream.Collectors.*;
 public class Sorting_Java8Streams {
 	private static final int MAX_STOCK_CAN_HOLD = 500;
 	private static final int MAX_NUM_OF_SHOE_TYPES = 10;
+	private static Stock stock = new Stock(new ArrayList<>());
 
 	public static void main(String[] args) {	
-		LoliShoeShop loliShoeShop = initLoliShoeShoeShop();	
-		List<Shoe> stockList = loliShoeShop.getStockList();
-		System.out.println("Items currently in stock: " + stockList.size());
+		//Init Stock				
+		StockToBeOrdered stockToBeOrdered = initStockTobeOrdered();	
+		
+		List<Shoe> stockToBeOrderedList = stockToBeOrdered .getStockToBeOrdered();
+		System.out.println("Size of stockToBeOrderedList: " + stockToBeOrderedList.size());
 		
 		Map<Supplier, List<Shoe>> shoesBySupplier
-		= stockList.stream().collect(Collectors.groupingBy(Shoe::getSupplier));
+		= stockToBeOrderedList.stream().collect(Collectors.groupingBy(Shoe::getSupplier));
 		
 		Map<Supplier, List<Shoe>> shoesBySuppliertPriceOver9000Cents
-		= stockList.stream().collect(Collectors.groupingBy(Shoe::getSupplier, Collectors
+		= stockToBeOrderedList.stream().collect(Collectors.groupingBy(Shoe::getSupplier, Collectors
 				.filtering(s -> s.getPriceInCents() > 9000, Collectors.toList())));
 		
 		
 		//Most expensive shoe per supplier
 		Map<Supplier, Optional<Shoe>> mostExpensiveShoeBySupplier
-		= stockList.stream().collect(groupingBy(Shoe::getSupplier, maxBy
+		= stockToBeOrderedList.stream().collect(groupingBy(Shoe::getSupplier, maxBy
 				(comparing(Shoe::getPriceInCents))));	
 	
 		
 		//Most expensive shoe per supplier - no optional	
 		Map<Supplier, Shoe> mostExpensiveShoeBySupplierNoOptional
-		= stockList.stream().collect(groupingBy(Shoe::getSupplier
+		= stockToBeOrderedList.stream().collect(groupingBy(Shoe::getSupplier
 				, collectingAndThen(maxBy
 				(comparing(Shoe::getPriceInCents)), Optional::get)));	
 		
 
 		//Max shoe price by supplier
 		Map<Supplier, Integer> maxShoePriceBySupplier
-		= stockList.stream().collect(groupingBy(Shoe::getSupplier, 
+		= stockToBeOrderedList.stream().collect(groupingBy(Shoe::getSupplier, 
 				collectingAndThen(maxBy(comparing(Shoe::getPriceInCents)), s -> s.get().getPriceInCents())));	
 		
 		
 		//Get all prices by supplier
-		Map<Supplier, Set<Integer>> allPricesBySupplier = stockList.stream()
+		Map<Supplier, Set<Integer>> allPricesBySupplier = stockToBeOrderedList.stream()
 				.collect(groupingBy(Shoe::getSupplier 
 						,mapping(Shoe::getPriceInCents, toSet())));
 			
 		//Shoe ids by supplier - as a set
-		Map<Supplier, Set<Integer>> shoeIdsBySupplierSetMap = stockList
+		Map<Supplier, Set<Integer>> shoeIdsBySupplierSetMap = stockToBeOrderedList
 				.stream().collect(groupingBy(Shoe::getSupplier
 						,mapping(Shoe::getId, toSet())));
 		
 		
 		//Get average shoe price by supplier
-		Map<Supplier, Double> averageShoePriceBySupplier = stockList.stream()
+		Map<Supplier, Double> averageShoePriceBySupplier = stockToBeOrderedList.stream()
 				.collect(groupingBy(Shoe::getSupplier, averagingDouble(Shoe::getPriceInCents)));
 		
-		//Task 7 - NESTED MAPPING  shoe ids and their suppliers´ telephone numbers
-				Map<Integer, List<String>> supplierTelByShoeId = stockList.stream().collect
+		//NESTED MAPPING  shoe ids and their suppliers´ telephone numbers
+				Map<Integer, List<String>> supplierTelByShoeId = stockToBeOrderedList.stream().collect
 						(groupingBy(Shoe::getId
 								, mapping(Shoe::getSupplier
 									,mapping(Supplier::getAddress
@@ -85,7 +87,7 @@ public class Sorting_Java8Streams {
 	}
 	
 	//in reality, would get this info from database
-	private static StockToBeOrdered initLoliShoeShoeShop() { 
+	private static StockToBeOrdered initStockTobeOrdered() { 
 		Address nikeAddress = new Address(1, (short)14, "Paul Street", "Harbourne", "Birmingham"
 				, "England", "B62 0GF", "01214507656");
 		Address doctorMartinsAddress = new Address(12, (short)142, "Lavina Road", "Smethick"
@@ -115,27 +117,20 @@ public class Sorting_Java8Streams {
 		StockToBeOrdered stockToBeOrdered = new StockToBeOrderedShopA (stockToBeOrdererd, stock, trainer, 400
 				, MAX_STOCK_CAN_HOLD, MAX_NUM_OF_SHOE_TYPES);
 		
-		try {
-			loliShoeShop.orderStock(trainer, 50);
-			loliShoeShop.orderStock(flipFlops, 5);
-			loliShoeShop.orderStock(hikingBoots, 7);
-			loliShoeShop.orderStock(nikeAirtrainerShoe , 8);
-			loliShoeShop.orderStock(leatherbootsVans, 27);
-			loliShoeShop.orderStock(r9s, 50);
-			loliShoeShop.orderStock(balletShoes, 134);
-			loliShoeShop.orderStock(sandals, 113);
-			loliShoeShop.orderStock(slippers, 45);
-			loliShoeShop.orderStock(wellies, 61);	
+		
+		System.out.println(addAndGetFeedback(stockToBeOrdered, trainer, 50));
+		System.out.println(addAndGetFeedback(stockToBeOrdered, flipFlops, 5));
+		System.out.println(addAndGetFeedback(stockToBeOrdered, hikingBoots, 7));
+		System.out.println(addAndGetFeedback(stockToBeOrdered, nikeAirtrainerShoe, 8));
+		System.out.println(addAndGetFeedback(stockToBeOrdered, leatherbootsVans, 27));
+		System.out.println(addAndGetFeedback(stockToBeOrdered, r9s, 50));
+		System.out.println(addAndGetFeedback(stockToBeOrdered, balletShoes, 134));
+		System.out.println(addAndGetFeedback(stockToBeOrdered, sandals, 113));
+		System.out.println(addAndGetFeedback(stockToBeOrdered, slippers, 45));
+		System.out.println(addAndGetFeedback(stockToBeOrdered, wellies, 61));
 
-		} catch (TooManyTypesOfShoesException | TooMuchStockException e) {
-			e.getMessage();
-			System.out.println(e.getMessage());
-		}	
 		
-		
-		
-		
-		return loliShoeShop;
+		return stockToBeOrdered;
 	}
 
 }
